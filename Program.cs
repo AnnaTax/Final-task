997d68f6f276a5587b922374b0d61cd63fbe2689
Anna <ant-anya@yandex.ru>
2023-04-02 23:15:45 +0300

    Файл с программой

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write($"Введите элементы массива через пробел или запятую, или точку: \n");
+string[] array=Console.ReadLine()!.Split(' ',',','.');
+int count=0;
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length<=3)
+        {Console.WriteLine($"{array[i]}\n");
+        count++;}
+    if (count ==0)
+        Console.WriteLine($"Все элементы массива содержат больше 3 символов");
